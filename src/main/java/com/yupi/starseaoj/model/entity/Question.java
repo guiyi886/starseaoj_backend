5367a3cf0e76399f4ef7e723cf74ea88ef7ce735
guiyi <1677852098@qq.com>
2024-08-10 06:50:32 +0800

    改为随机id应对爬虫

--- src/main/java/com/yupi/starseaoj/model/entity/Question.java
+++ src/main/java/com/yupi/starseaoj/model/entity/Question.java
@@ -20,7 +20,7 @@ public class Question implements Serializable {
     /**
      * id
      */
-    @TableId(type = IdType.AUTO)
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
     /**
